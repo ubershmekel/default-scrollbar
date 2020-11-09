a43b2b503c087f277c3304e6048bf49c4e11bbb6
Yuval Greenfield <ygreenfield@google.com>
2020-11-09 09:09:28 -0800

    Avoid spamming the errors tab with repeat sheet failures

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
+const failedSheets = {};
 
 function getBackToDefaultScrollbars() {
     // https://superuser.com/questions/380629/is-there-a-way-to-disable-custom-webkit-scrollbars/1585452#1585452
 
     for (var sheetI = 0; sheetI < document.styleSheets.length; ++sheetI) {
         var sheet = document.styleSheets[sheetI];
+        if (failedSheets[sheet.href]) {
+            continue;
+        }
         try {
             var ruleSet = sheet.rules || sheet.cssRules;
             for (var i = 0; i < ruleSet.length; ++i) {
@@ -15,6 +19,7 @@ function getBackToDefaultScrollbars() {
             }
         } catch (e) {
             console.warn("Can't read the css rules of: " + sheet.href, e);
+            failedSheets[sheet.href] = true;
         }
     };
 }
